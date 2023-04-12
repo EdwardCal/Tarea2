cd96de38e4987e96e26bd77cdbf104943b5f3cc5
Gilberth Chaves A <gchavesav@gmail.com>
2023-04-12 10:33:48 -0600

    IF-3001. I-2023. Lab4

--- src/main/java/domain/SinglyLinkedList.java
+++ src/main/java/domain/SinglyLinkedList.java
@@ -1,2 +1,94 @@
-package domain;public class SinglyLinkedList {
+package domain;
+
+public class SinglyLinkedList implements List {
+
+    @Override
+    public int size() throws ListException {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object element) throws ListException {
+        return false;
+    }
+
+    @Override
+    public void add(Object element) {
+
+    }
+
+    @Override
+    public void addFirst(Object element) {
+
+    }
+
+    @Override
+    public void addLast(Object element) {
+
+    }
+
+    @Override
+    public void addInSortedList(Object element) {
+
+    }
+
+    @Override
+    public void remove(Object element) throws ListException {
+
+    }
+
+    @Override
+    public Object removeFirst() throws ListException {
+        return null;
+    }
+
+    @Override
+    public Object removeLast() throws ListException {
+        return null;
+    }
+
+    @Override
+    public void sort() throws ListException {
+
+    }
+
+    @Override
+    public int indexOf(Object element) throws ListException {
+        return 0;
+    }
+
+    @Override
+    public Object getFirst() throws ListException {
+        return null;
+    }
+
+    @Override
+    public Object getLast() throws ListException {
+        return null;
+    }
+
+    @Override
+    public Object getPrev(Object element) throws ListException {
+        return null;
+    }
+
+    @Override
+    public Object getNext(Object element) throws ListException {
+        return null;
+    }
+
+    @Override
+    public Node getNode(int index) throws ListException {
+        return null;
+    }
 }
--- src/test/java/domain/SinglyLinkedListTest.java
+++ src/test/java/domain/SinglyLinkedListTest.java
@@ -1,4 +1,13 @@
+package domain;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class SinglyLinkedListTest {
-    
+
+    @Test
+    public void test() {
+
+    }
 }
